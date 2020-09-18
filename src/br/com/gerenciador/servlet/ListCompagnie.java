ebdf0db13e0f2208ce566e94a9320b5c9348da49
Kevin Souza <kevinsou113@gmail.com>
2020-09-18 12:53:08 -0300

    ListCompagnie 	Método doGet alterado para service devido interação nova da JSP newRegister.

--- src/br/com/gerenciador/servlet/ListCompagnie.java
+++ src/br/com/gerenciador/servlet/ListCompagnie.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 public class ListCompagnie extends HttpServlet {
 	private static final long serialVersionUID = 1L;
  
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		DataBase db = new DataBase();
 		List<Compagnie> list = db.getCompagnie();
